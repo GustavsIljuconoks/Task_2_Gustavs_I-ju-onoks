e05c06d4ecda78cfb24d40ccd91276002ae17c0b
GustavsIljuconoks <gustavs.iljuconoks@gmail.com>
2021-12-04 22:08:03 +0200

    mobile and desktop tweaks

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,9 +1,13 @@
+// Toggle hamburger menu
 const navSlide = () => {
     const burger = document.querySelector('.burger');
+    const burgerX = document.querySelector('.burgerX')
     const nav = document.querySelector('.menu');
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('menu-active');
+        // Animate burger
+        burger.classList.toggle('toogle');
 
     });
 }
